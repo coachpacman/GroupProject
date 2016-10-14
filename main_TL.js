ce7a9f971ade175c3fb3f2015420a633daf468da
Tim Lee <TLee@Tims-MacBook-Air.local>
2016-10-13 19:15:05 -0700

    added map and restaurant apis

--- main_TL.js
+++ main_TL.js
@@ -1,1 +1,17 @@
-console.log("test")
+$(document).ready(function() {
+	//NEWS API 
+	$.get("https://json-data.herokuapp.com/restaurant/news/1",function(news) {
+		console.log(news)
+	})
+
+	//SPECIALS MENU API
+	$.get("https://json-data.herokuapp.com/restaurant/special/1", function(specials) {
+		console.log(specials)
+	})
+
+	//ENTRES, APPETIZERS, ALA CARTE MENU ITEMS API
+	$.get("https://json-data.herokuapp.com/restaurant/menu/1", function(menuItems) {
+		console.log(menuItems)
+	})
+
+})
